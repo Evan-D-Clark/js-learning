5dd96fd15ad14e71b7b44ef80220cb8cf592fffd
Evan-D-Clark <eclark565678@gmail.com>
2021-07-15 11:08:26 -0700

    check speed

--- index.js
+++ index.js
@@ -28,26 +28,44 @@
 // console.log(isLandscape(10, 10));
 
 //fizzbuzz
+// function fizzBuzz(input) {
+//     let ret = input;
+//     if(typeof input !== 'number')
+//         ret = NaN;
+//     else
+//     {
+//         if(input % 3 === 0 && input % 5 === 0)
+//             ret = 'FizzBuzz';
+//         else if(input % 3 === 0)
+//             ret =  'Fizz';
+//         else if(input % 5 === 0)
+//             ret =  'Buzz';
+//     }
+//     return ret;
+// }
 
-function fizzBuzz(input) {
-    let ret = input;
-    if(typeof input !== 'number')
-        ret = 'Not a number'
-    else
-    {
-        if(input % 3 === 0 && input % 5 === 0)
-            ret = 'FizzBuzz';
-        else if(input % 3 === 0)
-            ret =  'Fizz';
-        else if(input % 5 === 0)
-            ret =  'Buzz';
+// console.log(fizzBuzz(30));
+// console.log(fizzBuzz(9));
+// console.log(fizzBuzz(25));
+// console.log(fizzBuzz(82));
+// console.log(fizzBuzz(true));
+// console.log(fizzBuzz('foobar'));
+
+// check speed
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    let ret = 'Ok';
+    if(speed >= speedLimit + kmPerPoint) {
+        ret = Math.floor((speed - speedLimit) / kmPerPoint);
+        if(ret >= 12)
+            ret = 'Liscense suspended';
     }
     return ret;
 }
 
-console.log(fizzBuzz(30));
-console.log(fizzBuzz(9));
-console.log(fizzBuzz(25));
-console.log(fizzBuzz(82));
-console.log(fizzBuzz(true));
-console.log(fizzBuzz('foobar'));
+console.log(checkSpeed(70));
+console.log(checkSpeed(74));
+console.log(checkSpeed(45));
+console.log(checkSpeed(90));
+console.log(checkSpeed(180));
