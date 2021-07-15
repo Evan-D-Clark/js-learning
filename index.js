b07598415ea25a3153f9bca23dba4ae80ef98c06
Evan-D-Clark <eclark565678@gmail.com>
2021-07-15 10:42:15 -0700

    Max of two numbers

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Swapping Variables
-let a = 'red';
-let b = 'blue';
+// let a = 'red';
+// let b = 'blue';
 
-let c = 'red';
-a = b;
-b = c;
+// let c = 'red';
+// a = b;
+// b = c;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+// Max of two numbers
+function maxOfTwoNumbers(a, b) {
+    return a > b ? a : b;
+}
+
+console.log(maxOfTwoNumbers(5, 10));
+console.log(maxOfTwoNumbers(10, 5));
+console.log(maxOfTwoNumbers(5, 5));
