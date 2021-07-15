475cc7337ff89b45a08b7df91d56a60859aaeaad
Evan-D-Clark <eclark565678@gmail.com>
2021-07-15 10:44:32 -0700

    Is landscape

--- index.js
+++ index.js
@@ -10,10 +10,18 @@
 // console.log(b);
 
 // Max of two numbers
-function maxOfTwoNumbers(a, b) {
-    return a > b ? a : b;
+// function maxOfTwoNumbers(a, b) {
+//     return a > b ? a : b;
+// }
+
+// console.log(maxOfTwoNumbers(5, 10));
+// console.log(maxOfTwoNumbers(10, 5));
+// console.log(maxOfTwoNumbers(5, 5));
+
+function isLandscape(width, height) {
+    return width > height;
 }
 
-console.log(maxOfTwoNumbers(5, 10));
-console.log(maxOfTwoNumbers(10, 5));
-console.log(maxOfTwoNumbers(5, 5));
+console.log(isLandscape(5, 10));
+console.log(isLandscape(10, 5));
+console.log(isLandscape(10, 10));
