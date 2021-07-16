884f8b566b4b8a337a6c8d2fa1a357ca75677f2f
Evan-D-Clark <eclark565678@gmail.com>
2021-07-16 16:14:37 -0700

    array from range

--- extra.js
+++ extra.js
@@ -1,8 +1,19 @@
-function UnfinishedPost(title, body, author) {
-    this.title = title;
-    this.body = body;
-    this.author = author;
-    this.views = 0;
-    this.comments = [];
-    this.isLive = false;
+// second constructor exercise
+// function UnfinishedPost(title, body, author) {
+//     this.title = title;
+//     this.body = body;
+//     this.author = author;
+//     this.views = 0;
+//     this.comments = [];
+//     this.isLive = false;
+// }
+
+// array from range
+function ArrayFromRange(start, end) {
+    const ret = [];
+    for(let ii = start; ii <= end; ++ii)
+        ret.push(ii);
+    return ret;
 }
+
+console.log(ArrayFromRange(4, 10));
