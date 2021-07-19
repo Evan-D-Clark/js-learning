822b4c99d44b67e3acd1af194305c552918ebde0
Evan-D-Clark <eclark565678@gmail.com>
2021-07-19 10:00:06 -0700

    ES6 classes

--- advanced.js
+++ advanced.js
@@ -29,44 +29,76 @@
 // let sw = new StopWatch();
 
 // prototypical inheritance and polymorphism
-function HtmlElement() {
-    this.click = function() {
-        console.log('clicked');
+// function HtmlElement() {
+//     this.click = function() {
+//         console.log('clicked');
+//     }
+// }
+
+// HtmlElement.prototype.focus = function() {
+//     console.log('focused');
+// }
+
+// function HtmlSelectElement(items = []) {
+//     this.items = items;
+
+//     this.addItem = function(item) {
+//         this.items.push(item);
+//     }
+
+//     this.removeItem = function(item) {
+//         this.items.splice(this.items.indexOf(item), 1);
+//     }
+
+//     this.render = function() {
+//         return `
+// <select>${this.items.map(item => `
+//     <option>${item}</option>`).join('')}
+// </select>`;
+//     }
+// }
+
+// HtmlSelectElement.prototype = new HtmlElement();
+// HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+// // polymorphism
+// function HtmlImageElement(src) {
+//     this.src = src;
+//     this.render = function() {
+//         return `<img src="${this.src}" />`;
+//     }
+// }
+// HtmlImageElement.prototype = HtmlElement();
+// HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+// ES6 classes
+const _items = new WeakMap();
+class Stack {
+    constructor() {
+        _items.set(this, []);
+    }
+
+    push(obj) {
+        _items.get(this).push(obj);
+    }
+
+    pop() {
+        const items = _items.get(this);
+        if(items.length === 0)
+            throw new Error('Stack is empty.');
+        return _items.pop();
+    }
+
+
+    peek() {
+        const items = _items.get(this);
+        if(items.length === 0) {
+            throw new Error('Stack is empty.');
+        }
+        return items[items.length - 1];
+    }
+
+    get count() {
+        return _items.get(this).length;
     }
 }
-
-HtmlElement.prototype.focus = function() {
-    console.log('focused');
-}
-
-function HtmlSelectElement(items = []) {
-    this.items = items;
-
-    this.addItem = function(item) {
-        this.items.push(item);
-    }
-
-    this.removeItem = function(item) {
-        this.items.splice(this.items.indexOf(item), 1);
-    }
-
-    this.render = function() {
-        return `
-<select>${this.items.map(item => `
-    <option>${item}</option>`).join('')}
-</select>`;
-    }
-}
-
-HtmlSelectElement.prototype = new HtmlElement();
-HtmlSelectElement.prototype.constructor = HtmlSelectElement;
-
-// polymorphism
-function HtmlImageElement(src) {
-    this.src = src;
-    this.render = function() {
-        return `<img src="${this.src}" />`;
-    }
-}
-HtmlImageElement.prototype = HtmlElement();
-HtmlImageElement.prototype.constructor = HtmlImageElement;
